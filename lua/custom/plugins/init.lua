86736799cfad487f2f3ef781b9bd1e2eed6f3691
Anil Dhurjaty <anil.dhurjaty@appfolio.com>
2025-06-09 21:25:42 -0700

    Organize vim plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,14 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  'tpope/vim-fugitive',
-  {
-    'voldikss/vim-floaterm',
-    config = function()
-      vim.keymap.set('n', '<leader>lg', '<CMD>FloatermNew lazygit<CR>', { desc = 'Open [l]azy[g]it in floaterm' })
-      vim.g.floaterm_width = 0.9
-      vim.g.floaterm_height = 0.7
-    end,
-  },
-}
+return {}
